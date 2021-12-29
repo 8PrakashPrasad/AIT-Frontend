7803125edcb6456ee9e00ab606fad7dbbaa2457b
8PrakashPrasad <prakashprasad_20616@aitpune.edu.in>
2021-12-30 01:04:36 +0530

    Front-End of AIT

---
+++ Form-Handler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'prakashprasad142002@gmail.com'
+
+$email_subject = 'New For Submission'
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                  "Subject: $subject.\n".
+                  "User Message: $message.\n";
+
+$to = 'deathcaptaingaming19@gmail.com';
+
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+?>
